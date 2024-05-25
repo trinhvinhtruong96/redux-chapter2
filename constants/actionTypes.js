3a0bcc1c0380dc8fb774a720e1fc73a4adaf02f6
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2024-05-25 15:10:55 +0700

    Update actionTypes.js

--- constants/actionTypes.js
+++ constants/actionTypes.js
@@ -5,9 +5,13 @@ export const SET_RECIPES = 'SET_RECIPES';
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const SET_INGREDIENTS = 'SET_INGREDIENTS';
 
-/* const asyncActionType = (type) => ({
+/* 1 const asyncActionType = (type) => ({
   2 PENDING: `${type}_PENDING`,
   3 SUCCESS: `${type}_SUCCESS`,
   4 ERROR: `${type}_ERROR`,
-  5 }); */
+  5 });
+  6
+  7 export const LOGIN = asyncActionType('LOGIN');
+  8 export const FETCH_RECIPES = asyncActionType('FETCH_RECIPES'); */
+  
   
